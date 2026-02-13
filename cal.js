ecb66322418c342d13aadc634911cbb4842649e0
raphael mondo <raphaelmondo15@gmail.com>
2026-02-13 13:23:18 +0100

    adding a placeholder output

---
+++ cal.js
@@ -0,0 +1,22 @@
+let result=document.getElementById('inputtext');
+const answer = document.getElementById('output');
+let appendValue = (number) => {
+    result.value += number;
+}
+
+let Result = () => {
+    try {
+        answer.innerText = eval(result.value);
+    } catch (error) {
+      alert("error");
+    }
+}
+
+let deleteLast= () => {
+    result.value = result.value.slice(0, -1);
+}
+
+function clr() {
+    result.value = '';
+    answer.innerText = '0';
+}
